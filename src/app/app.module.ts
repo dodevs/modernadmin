b48b1d4203bd5a678baf27afe776a3c0f4c03747
dodevs <douglass.sousa@outlook.com.br>
2018-08-30 13:32:12 -0300

    Importado componente e rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import * as $ from 'jquery';
 import { UiModule } from './ui/ui.module';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app.router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     UiModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
