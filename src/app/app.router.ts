e491d86a38751f01fcbe2ddea425a1e17dd01957
dodevs <douglass.sousa@outlook.com.br>
2018-08-30 13:30:35 -0300

    Modulo de rota

---
+++ src/app/app.router.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'usuarios', component: UsuariosComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
