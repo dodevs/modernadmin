804ac40bb62d2f6a78bfb9e75a6f9980ccd1acb7
dodevs <douglass.sousa@outlook.com.br>
2018-08-30 12:45:43 -0300

    Adiconado componente Usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import * as $ from 'jquery';
 import { UiModule } from './ui/ui.module';
 import { SharedModule } from './shared/shared.module';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
